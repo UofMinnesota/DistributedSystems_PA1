bcdba57e4051df369a5b051db4d3877f69fdbe0f
Bernardo <godin040@umn.edu>
2016-02-28 16:01:31 -0600

    first

--- gen-java/project1/SuperNodeServiceHandler.java
+++ gen-java/project1/SuperNodeServiceHandler.java
@@ -1,14 +1,29 @@
-package com.eviac.blog.samples.thrift.server;
+package project1;
 
 import org.apache.thrift.TException;
 
-public class AdditionServiceHandler implements AdditionService.Iface {
+public class SuperNodeServiceHandler implements SuperNodeService.Iface {
 
  @Override
  public String Join(String IP, int Port) throws TException {
-  String NodeList;
+  String NodeList = " ";
 
   return NodeList;
  }
+
+ @Override
+ public String GetNode() throws TException {
+  String NodeList = " ";
+
+  return NodeList;
+ }
+
+ @Override
+ public boolean PostJoin(String IP, int Port) throws TException {
+
+
+  return false;
+ }
+
 
 }
