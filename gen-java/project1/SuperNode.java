2b9e6003e29dbf931c71ac54056655b0bb9c67b1
Sathishkumar Vijayakumar <vijay072@umn.edu>
2016-02-28 16:25:02 -0600

    supernode java

---
+++ gen-java/project1/SuperNode.java
@@ -0,0 +1,32 @@
+package project1;
+
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TServer.Args;
+import org.apache.thrift.server.TSimpleServer;
+
+public class SuperNode {
+
+ public static void StartsimpleServer(SuperNodeService.Processor<SuperNodeServiceHandler> processor) {
+  try {
+   TServerTransport serverTransport = new TServerSocket(9090);
+   TServer server = new TSimpleServer(
+     new Args(serverTransport).processor(processor));
+
+   // Use this for a multithreaded server
+   // TServer server = new TThreadPoolServer(new
+   // TThreadPoolServer.Args(serverTransport).processor(processor));
+
+   System.out.println("Starting SuperNode...");
+   server.serve();
+  } catch (Exception e) {
+   e.printStackTrace();
+  }
+ }
+ 
+ public static void main(String[] args) {
+  StartsimpleServer(new SuperNodeService.Processor<SuperNodeServiceHandler>(new SuperNodeServiceHandler()));
+ }
+
+}
