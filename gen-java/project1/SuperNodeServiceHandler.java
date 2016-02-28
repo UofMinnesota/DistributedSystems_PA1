dc63f4ad4878f3698071dbb8546ef911501b67f5
Sathishkumar Vijayakumar <vijay072@umn.edu>
2016-02-28 15:50:06 -0600

    initialized repo for project1

---
+++ gen-java/project1/SuperNodeServiceHandler.java
@@ -0,0 +1,14 @@
+package com.eviac.blog.samples.thrift.server;
+
+import org.apache.thrift.TException;
+
+public class AdditionServiceHandler implements AdditionService.Iface {
+
+ @Override
+ public String Join(String IP, int Port) throws TException {
+  String NodeList;
+
+  return NodeList;
+ }
+
+}
