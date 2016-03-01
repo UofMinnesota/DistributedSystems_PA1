f4380d1dc03c52a49b679860107da6f5ba454c23
Bernardo <godin040@umn.edu>
2016-03-01 00:42:54 -0600

    node can parse nodelist

--- gen-java/project1/NodeServiceHandler.java
+++ gen-java/project1/NodeServiceHandler.java
@@ -2,9 +2,41 @@ package project1;
 
 import org.apache.thrift.TException;
 import java.security.*;
+import java.util.ArrayList;
+import java.util.Random;
+
 
 public class NodeServiceHandler implements NodeService.Iface {
 
+  public class  NodeInfo{
+    String address = "";
+    int port = 0;
+    int hash = 0;
+  }
+
+  ArrayList<NodeInfo> ListOfNodes = new ArrayList<NodeInfo>();
+
+  public NodeInfo strToNodeInfo(String input)
+  {
+    String data[] = input.split(":");
+    NodeInfo newNo = new NodeInfo();
+    newNo.address = data[0];
+    newNo.port = Integer.parseInt(data[1]);
+    newNo.hash = Integer.parseInt(data[2]);
+
+    return newNo;
+  }
+
+  public ArrayList<NodeInfo> strToNodeInfoArray(String input)
+  {
+    ArrayList<NodeInfo> arrN = new ArrayList<NodeInfo>();
+    String data[] = input.split(",");
+    for(int c = 0; c < data.length; c++)
+    {
+      arrN.add(strToNodeInfo(data[c]));
+    }
+    return arrN;
+  }
 
   public String MD5(String md5) {
      try {
@@ -16,7 +48,7 @@ public class NodeServiceHandler implements NodeService.Iface {
          }
           return sb.toString();
       } catch (java.security.NoSuchAlgorithmException e) {
-      
+
       return null;}
   }
 
